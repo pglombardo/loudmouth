b80a386fa4dc2db3890bb949e9659869386e8e0e
Peter Giacomo Lombardo <pglombardo@gmail.com>
2011-06-08 23:19:57 -0400

    Placeholder extentions.  Currently empty.

---
+++ lib/extensions/helper.rb
@@ -0,0 +1,17 @@
+module Loudmouth
+  module Controllers
+    module Helpers
+      extend ActiveSupport::Concern
+      included do
+        # a placeholder for the time being
+      end
+
+    end
+    module InternalHelpers #:nodoc:
+      extend ActiveSupport::Concern
+      included do
+        # a placeholder for the time being
+      end
+    end
+  end
+end
