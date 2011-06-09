afbcf1ce97e60e86db630ee38f315189986ab096
Peter Giacomo Lombardo <pglombardo@gmail.com>
2011-06-09 11:37:59 -0400

    Updated initializer template with new_comment_content. Loudmouth module definition.

--- lib/generators/loudmouth/templates/loudmouth_initializer.rb
+++ lib/generators/loudmouth/templates/loudmouth_initializer.rb
@@ -1,5 +1,9 @@
 
 Loudmouth.setup do |config|
+  # Optionally specify a layout to use when rendering comments#index
   config.use_layout = ''
+  
+  # Default comment text for the new comment form
+  config.new_comment_content = "Enter new Comment..."
 end
 
---
+++ lib/loudmouth.rb
@@ -0,0 +1,18 @@
+require "loudmouth"
+require "rails/routes"
+require "extensions/helper"
+
+module Loudmouth
+  class Engine < Rails::Engine
+  end
+  
+  mattr_accessor :use_layout
+  @@use_layout = ""
+  
+  mattr_accessor :new_comment_content
+  @@new_comment_content = ""
+
+  def self.setup
+    yield self
+  end  
+end
