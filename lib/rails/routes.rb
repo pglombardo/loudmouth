2d1fc65b445b56036403ee10ce10909e82590451
Peter Giacomo Lombardo <pglombardo@gmail.com>
2011-06-09 16:41:19 -0400

    Change options handling so it acts as just a pass through. We extract/delete loudmouth specific options prior to passing it on.

--- lib/rails/routes.rb
+++ lib/rails/routes.rb
@@ -3,8 +3,7 @@ module ActionDispatch::Routing
 
     def comments_on(*resources)
       options = resources.extract_options!
-      controller = 'loudmouth/comments'
-      
+
       unless resources.count == 1
         raise "too many arguments passed to comments_on.  Format is comments_on :articles, :by => :users"
       end
@@ -13,17 +12,20 @@ module ActionDispatch::Routing
         raise "comments_on must include an :by option.  Format is comments_on :articles, :by => :users"
       end
       
-      controller = options[:controller] if options.has_key?(:controller)
+      unless options.has_key?(:controller)
+        options[:controller] = 'loudmouth/comments'
+      end
       
       topic_model = resources.first.to_s
       user_model = options[:by].to_s
+      options.delete(:by)
       
       constraint = lambda { |req| req.env["loudmouth_map"] = { :topic_model => topic_model, 
                                                                :user_model => user_model }; true }
       Rails.application.routes.draw do
         resources topic_model do
           constraints(constraint) do
-            resources "comments", :controller => controller
+            resources "comments", options
           end
         end
       end
