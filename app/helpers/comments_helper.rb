bab4eb9c56060b86be1cf015081b177c9415f018
Peter Giacomo Lombardo <pglombardo@gmail.com>
2011-06-12 11:18:45 -0400

    render_comment_form no longer needed.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,4 @@
 module CommentsHelper
-  def render_comment_form
-    render :partial => 'loudmouth/comments/comment_form'
-  end
   
   def topic_comment_path(topic, comment)
     self.send(topic + '_comment_path', topic, comment)
