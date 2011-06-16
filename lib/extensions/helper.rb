a8a5ec7faa5bea67d6af9f285593d7e5084b9982
Peter Giacomo Lombardo <pglombardo@gmail.com>
2011-06-16 13:47:50 -0400

    Initial implementation of render_comments_for. Include the Sanitize helper. downcase not required.

--- lib/extensions/helper.rb
+++ lib/extensions/helper.rb
@@ -2,13 +2,14 @@ module Loudmouth
   module Controllers
     module Helpers
       extend ActiveSupport::Concern
+      include ActionView::Helpers::SanitizeHelper
       
       included do
-        helper_method :render_comment_form_for
+        # helper_method :render_comment_form_for, :render_comments_for
       end
       
       def render_comment_form_for(topic, user)
-        unless Loudmouth.treasure_map.has_key?(topic.class.name.underscore.downcase.to_sym)
+        unless Loudmouth.treasure_map.has_key?(topic.class.name.underscore.to_sym)
           raise "loudmouth not setup for #{topic.class.name.underscore.downcase.to_sym}"
         end
 
@@ -16,8 +17,16 @@ module Loudmouth
                 :locals => { :topic => topic, :user => user, :content => Loudmouth.new_comment_content }
       end
       
+      def render_comments_for(topic)
+        unless Loudmouth.treasure_map.has_key?(topic.class.name.underscore.to_sym)
+          raise "loudmouth not setup for #{topic.class.name.underscore.downcase.to_sym}"
+        end
+
+        render :partial => 'loudmouth/comments/comment', :collection => topic.comments, :as => :comment
+      end
+      
       def topic_comment_path(topic)
-        send("#{topic.class.name.underscore.downcase}_comments_path", topic)
+        send("#{topic.class.name.underscore}_comments_path", topic)
       end
       
     end
