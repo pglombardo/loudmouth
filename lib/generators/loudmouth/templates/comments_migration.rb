3388db8444d50509caa3f6798695d8c099f0a3b5
Peter Giacomo Lombardo <pglombardo@gmail.com>
2011-06-18 11:12:57 -0400

    Include table indexes in the template migration. Use foreign key to generate key columns.

--- lib/generators/loudmouth/templates/comments_migration.rb
+++ lib/generators/loudmouth/templates/comments_migration.rb
@@ -2,14 +2,20 @@ class Create<%= topic_model.camelize %>Comments < ActiveRecord::Migration
   def self.up
     create_table :<%= topic_model.downcase %>_comments do |t|
       t.references :parent
-      t.integer :<%= topic_model.downcase %>_id
-      t.integer :author_<%= user_model.downcase %>_id
+      t.integer :<%= topic_model.downcase.foreign_key %>
+      t.integer :author_<%= user_model.downcase.foreign_key %>
       t.text :content
       t.timestamps
     end
+    add_index :<%= topic_model.downcase %>_comments, :<%= topic_model.downcase.foreign_key %>
+    add_index :<%= topic_model.downcase %>_comments, :author_<%= user_model.downcase.foreign_key %>
+    add_index :<%= topic_model.downcase %>_comments, :parent_id
   end
 
   def self.down
+    remove_index :<%= topic_model.downcase %>_comments, :parent_id
+    remove_index :<%= topic_model.downcase %>_comments, :author_<%= user_model.downcase.foreign_key %>
+    remove_index :<%= topic_model.downcase %>_comments, :<%= topic_model.downcase.foreign_key %>
     drop_table :<%= topic_model.downcase %>_comments
   end
 end
